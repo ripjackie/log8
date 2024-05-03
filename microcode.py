ec5f4e2c0bf39a7ace59ee9fa460426e9d28f41d
ripjackie <theranslinkard@gmail.com>
2024-05-03 11:50:39 -0500

    changed cpu, workin on py microcode

--- microcode.py
+++ microcode.py
@@ -1,44 +1,26 @@
-# ALU
-EN_OP0  = 0x01
-EN_OP1  = 0x02
-EN_OP2  = 0x04
-EN_CIN  = 0x08
-EN_ACC  = 0x10
-ST_ACC  = 0x20
-EN_ALU  = 0x40
-# RAM
-ST_RAM  = 0x80
-EN_RAM  = 0x0100
-# PC
-EN_PC16 = 0x0200
-EN_PC08 = 0x0400
-EN_PCLD = 0x0800
-EN_PCCT = 0x1000
-# REGISTER
-EN_RS0  = 0x2000
-EN_RS1  = 0x4000
-EN_RS2  = 0x8000
-ST_RX   = 0x010000
-EN_RX   = 0x020000
-EN_RHL  = 0x040000
-# CTRL
-ST_INR  = 0x080000
-ST_FLG  = 0x100000
-
-INST_STEP = [
-    EN_PC16|EN_RAM, EN_PC16|EN_PCCT|EN_RAM|ST_INR
-]
-instructions = bytearray([0x00]*0xff*0x10)
-
-INSTS = [
-    [EN_PC16|EN_RAM, EN_PC16|EN_RAM|EN_RX]
-]
-
-def write_microcode() -> None:
-    with open("./microcode.txt", "w") as f:
-        _ = f.write("v3.0 hex words addressed\n")
-
-
-if __name__ == "__main__":
-    print(instructions)
-    # write_microcode()
+ALU_CIN = 0x01
+ALU_OP0 = 0x02
+ALU_OP1 = 0x04
+ALU_OP2 = 0x08
+ACC_MPX = 0x10
+ACC_IN  = 0x20
+ACC_OUT = 0x40
+TMP_IN  = 0x80
+RAM_OUT = 0x0100
+RAM_IN  = 0x0200
+PC_DEC  = 0x0400
+PC_LD   = 0x0800
+PC_CT   = 0x1000
+PC_16   = 0x2000
+PC_08   = 0x4000
+SP_IN   = 0x8000
+SP_08   = 0x010000
+SP_16   = 0x020000
+HL_OUT  = 0x040000
+RX_OUT  = 0x080000
+RX_IN   = 0x100000
+RX0     = 0x200000
+RX1     = 0x400000
+RST     = 0x800000
+INR_IN  = 0x01000000
+DTA_OUT = 0x02000000

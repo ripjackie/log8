9b4946f773deab19d8ae280c7550e37eb3cb5772
ripjackie <theranslinkard@gmail.com>
2024-05-06 16:47:46 -0500

    new CPU version, added 4 more regs, 16x on each pair. upgraded PRG & STP, reorganized pins

--- microcode.py
+++ microcode.py
@@ -1,26 +1,70 @@
-ALU_CIN = 0x01
-ALU_OP0 = 0x02
-ALU_OP1 = 0x04
-ALU_OP2 = 0x08
-ACC_MPX = 0x10
-ACC_IN  = 0x20
-ACC_OUT = 0x40
-TMP_IN  = 0x80
-RAM_OUT = 0x0100
-RAM_IN  = 0x0200
-PC_DEC  = 0x0400
-PC_LD   = 0x0800
-PC_CT   = 0x1000
-PC_16   = 0x2000
-PC_08   = 0x4000
-SP_IN   = 0x8000
-SP_08   = 0x010000
-SP_16   = 0x020000
-HL_OUT  = 0x040000
-RX_OUT  = 0x080000
-RX_IN   = 0x100000
-RX0     = 0x200000
-RX1     = 0x400000
-RST     = 0x800000
-INR_IN  = 0x01000000
-DTA_OUT = 0x02000000
+import numpy as np
+
+# ALU
+sTMP   = 0x01
+eACC   = 0x02
+sACC   = 0x04
+sACCMx = 0x08
+eCIN   = 0x10
+eOP0   = 0x20
+eOP1   = 0x40
+eOP2   = 0x80
+# RAM
+sMAR   = 0x0100
+eRAM   = 0x0200
+sRAM   = 0x0400
+# PRG
+ePRG16 = 0x0800
+ePRG08 = 0x1000
+ePRGld = 0x2000
+ePRGct = 0x4000
+# STP
+eSTP16 = 0x8000
+eSTP08 = 0x010000
+sSTP   = 0x020000
+# RFILE
+sRX    = 0x040000
+eRX    = 0x080000
+eRXY   = 0x100000
+eRX0   = 0x200000
+eRX1   = 0x400000
+eRX2   = 0x800000
+# CTRL
+sDATA  = 0x01000000
+sINR   = 0x02000000
+sRST   = 0x04000000
+
+
+nxt = [ ePRG16|ePRGct|sMAR, eRAM|sINR ]
+
+instructions = [
+    # nop
+    [ sRST ],
+    # data ra, %NB
+    [ ePRG16|ePRGct|sMAR, eRAM|sRX, sRST ],
+    # data rb, %NB
+    [ ePRG16|ePRGct|sMAR, eRAM|sRX|eRX0, sRST ],
+    # data rl, %NB
+    [ ePRG16|ePRGct|sMAR, eRAM|sRX|eRX1, sRST ],
+    # data rh, %NB
+    [ ePRG16|ePRGct|sMAR, eRAM|sRX|eRX1|eRX0, sRST ],
+    # data rA, %NB %NB
+    [ ePRG16|ePRGct|sMAR, eRAM|sRX, ePRG16|ePRGct|sMAR, eRAM|sRX|eRX0, sRST ],
+    # data rH, %NB %NB
+    [ ePRG16|ePRGct|sMAR, eRAM|sRX|eRX1, ePRG16|ePRGct|sMAR, eRAM|sRX|eRX0|eRX1, sRST ],
+]
+
+def format():
+    for i, inst in enumerate(instructions):
+        instructions[i] = nxt + inst
+        instructions[i] += [0] * (16 - len(instructions[i]))
+
+if __name__ == "__main__":
+    format()
+    arr = np.zeros((256, 16), dtype=int)
+    print(instructions[0])
+    arr[0x00:0x10] = instructions[0]
+    arr[0x10:0x16] = instructions[1:7]
+
+    np.savetxt("./microcode2.txt", arr, fmt="%08x", delimiter=" ", header="v3.0 hex words plain", comments="")
+    print(arr)
